6056e9c939bf213eb790b02c59e28358c020f2d7
Bhavy Pan <panbhavy432@gmail.com>
2025-11-05 14:48:08 +0530

    Create url.js

---
+++ api/auth/url.js
@@ -0,0 +1,22 @@
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const baseUrl = process.env.VERCEL_URL 
+    ? `https://${process.env.VERCEL_URL}` 
+    : 'http://localhost:3000';
+  
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
+    `client_id=${process.env.GOOGLE_CLIENT_ID}&` +
+    `redirect_uri=${baseUrl}/api/token&` +
+    `response_type=code&` +
+    `scope=https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send&` +
+    `access_type=offline&` +
+    `prompt=consent`;
+
+  res.status(200).json({ 
+    authUrl,
+    baseUrl 
+  });
+}
